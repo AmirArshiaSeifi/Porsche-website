2fa3012a86da108e1f694c28843b9507b6f3a83a
AmirArshiaSeifi <AmirArshia.seifee@gmail.com>
2025-09-15 23:30:48 +0330

    initialize git

---
+++ pages/index.php
@@ -0,0 +1,87 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/styleHeader.css">
+</head>
+<body>
+    <?php include("header.html"); ?>
+    <div class="scroll-watcher"></div>
+    <div class="top-container">
+        <div class="image-top">
+    
+            <div class="fade">
+            </div>
+        </div>
+        
+        <div class="products">
+
+            <div class="inner">
+                <div class="inner-image1"></div>
+                <div class="inner-text">
+                    <p>Porsche Classic Care Set</p>
+                    <p>The best care for your Porsche classic is offered by the 17-piece care set in a high-quality storage bag</p>
+                </div>
+            </div>
+            <div class="inner">
+                <div class="inner-image2"></div>
+                <div class="inner-text">
+                    <p>Porsche Poster Set - 75 years</p>
+                    <p>Powerful motifs and high-quality printing: the limited-edition poster sets portray various Porsche models.</p>
+                </div>
+            </div>
+            <div class="inner">
+                <div class="inner-image3"></div>
+                <div class="inner-text">
+                    <p>Sunglasses P 8478</p>
+                    <p>The Porsche Design men's sunglasses with interchangeable lenses are a design classic.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <p class="text-top">Porsche, a symbol of luxury, performance, and innovation in the automotive industry, offers a range of sports cars, SUVs, and advanced electric vehicles that combine exceptional design, cutting-edge technology, and an unparalleled driving experience.</p>
+
+    <div class="container">
+        <div class="int a">
+            <!-- <img class="img a" src="../images/1.avif"> -->
+            <p class="name">718</p>
+            <p class="p">The mid-engine sports car for two made for pure driving pleasure.</p>
+            <div class="shadow"></div>
+        </div>
+        <div class="int b">
+            <!-- <img class="img b" src="../images/2.avif"> -->
+            <p class="name">911</p>
+            <p class="p">The iconic, rear-engine sports car with exceptional performance.</p>
+            <div class="shadow"></div>
+        </div>
+        <div class="int c">
+            <!-- <img class="img c" src="../images/3.avif"> -->
+            <p class="name">Taycan</p>
+            <p class="p">The pure expression of an electric sports car with motorsport performance.</p>
+            <div class="shadow"></div>
+        </div>
+        <div class="int d">
+            <!-- <img class="img d" src="../images/4.avif"> -->
+            <p class="name">Panamera</p>
+            <p class="p">The sports car limousine for an active lifestyle with highest comfort.</p>
+            <div class="shadow"></div>
+        </div>
+        <div class="int e">
+            <!-- <img class="img e" src="../images/5.avif"> -->
+            <p class="name">Macan</p>
+            <p class="p">The sports car of compact SUVs.</p>
+            <div class="shadow"></div>
+        </div>
+        <div class="int f">
+            <!-- <img class="img f" src="../images/6.avif"> -->
+            <p class="name">Cayenne</p>
+            <p class="p">The versatile SUV with sports car performance and up to five seats.</p>
+            <div class="shadow"></div>
+        </div>
+    </div>
+
+</body>
+</html>
